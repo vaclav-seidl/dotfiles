f4e31036c2a2d50a38912fbca99cbcd29b98c9a4
Václav Seidl <vaclav.seidl@inventing-animals.com>
2026-05-14 13:04:26 +0300

    Vim: Configuration
    Some configuration could go directly into the plugin files when I learn
    how to do it

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Plugin manager
+require('config.lazy')
+require("lazy").setup("plugins")
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
